0477e1a626e465d98ca93f7ec1652415ef5a582e
Thi-Ponce <thi.pponce@gmail.com>
2022-02-23 17:48:37 -0300

    Delete test

--- src/Add&Delete.test.js
+++ src/Add&Delete.test.js
@@ -35,3 +35,38 @@ describe("DOM manipulation", () => {
     expect(tasks.length).toBe(3);
   });
 });
+
+describe('ToDoList localstorage'), () => {
+  test('Delete', () => {
+    const ToDoList = new TaskList();
+    ToDoList.AddToDO('New Task 1');
+    ToDoList.AddToDO('New Task 2');
+    ToDoList.deleteTask(1);
+    expect(ToDoList.ToDo.length).toBe(1);
+  });
+};
+
+describe('DOM manipulation delete'), () => {
+  document.body.innerHTML = `
+    <template id="ToDo" >
+      <div class="row toDoR" draggable="true">
+        <div class="task">
+          <input type="checkbox" class="checkbox">
+          <input class="toDoName" type="text">
+        </div>
+        <button class='btnMove' type="button">&#8942;</button>
+        <button class='btnRemove' type="button">&#128465;</button>
+      </div>
+    </template>
+    <div class="container">
+    </div>
+    `;
+    window.localStorage.clear();
+    const List = new Container();
+    List.ToDoList.AddToDO("event");
+    List.ToDoList.AddToDO("event 1");
+    List.ToDoList.RemoveToDo(0);
+    List.ShowList();
+    const tasks = document.body.querySelectorAll('.toDoR')
+    expect(tasks.length).toBe(1);
+};
