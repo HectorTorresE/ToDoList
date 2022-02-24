a500eb89bcf716b99c21cc170be3c7ca0f06c14c
Thi-Ponce <thi.pponce@gmail.com>
2022-02-24 12:36:05 -0300

    Add DOM test

--- ToDoList.test.js
+++ ToDoList.test.js
@@ -100,3 +100,92 @@ describe('Test update title, clear all, and completed of to-do', () => {
     expect(size).toBe(1);
   });
 });
+
+describe('update DOM', () => {
+  test('update title in DOM', () => {
+    window.localStorage.clear();
+    document.body.innerHTML = `
+    <template id='ToDo' >
+    <div class='row toDoR' draggable='true'>
+    <div class='task'>
+    <input type='checkbox' class='checkbox'>
+    <input class='toDoName' type='text'>
+    </div>
+    <button class='btnMove' type='button'>&#8942;</button>
+    <button class='btnRemove' type='button'>&#128465;</button>
+    </div>
+    </template>
+    <div class='container'>
+    </div>
+    `;
+    const cont = new Container();
+    cont.List.AddToDO('event');
+    cont.List.AddToDO('event 1');
+    cont.List.AddToDO('event 2');
+    cont.List.AddToDO('event 3');
+    cont.ShowList();
+    cont.List.updateTitle(1, 'gym');
+    cont.ShowList();
+    const row = document.body.querySelectorAll('.toDoR');
+    const title = row[0].querySelector('.toDoName').value;
+    expect(title).toBe('gym');
+  });
+
+  test('status change in DOM', () => {
+    window.localStorage.clear();
+    document.body.innerHTML = `
+    <template id='ToDo' >
+    <div class='row toDoR' draggable='true'>
+    <div class='task'>
+    <input type='checkbox' class='checkbox'>
+    <input class='toDoName' type='text'>
+    </div>
+    <button class='btnMove' type='button'>&#8942;</button>
+    <button class='btnRemove' type='button'>&#128465;</button>
+    </div>
+    </template>
+    <div class='container'>
+    </div>
+    `;
+    const cont = new Container();
+    cont.List.AddToDO('event');
+    cont.List.AddToDO('event 1');
+    cont.List.AddToDO('event 2');
+    cont.ShowList();
+    cont.List.changeDone(1, true);
+    cont.ShowList();
+    const row = document.body.querySelectorAll('.toDoR');
+    const done = row[0].querySelector('.checkbox').checked;
+    expect(done).toBeTruthy();
+  });
+
+  test('test clear all in DOM', () => {
+    window.localStorage.clear();
+    document.body.innerHTML = `
+    <template id='ToDo' >
+    <div class='row toDoR' draggable='true'>
+    <div class='task'>
+    <input type='checkbox' class='checkbox'>
+    <input class='toDoName' type='text'>
+    </div>
+    <button class='btnMove' type='button'>&#8942;</button>
+    <button class='btnRemove' type='button'>&#128465;</button>
+    </div>
+    </template>
+    <div class='container'>
+    </div>
+    `;
+    const cont = new Container();
+    cont.List.AddToDO('event');
+    cont.List.AddToDO('event 1');
+    cont.List.AddToDO('event 2');
+    cont.ShowList();
+    cont.List.changeDone(1, true);
+    cont.List.changeDone(2, true);
+    cont.ShowList();
+    cont.List.removeCompleted();
+    cont.ShowList();
+    const row = document.body.querySelectorAll('.toDoR');
+    expect(row.length).toBe(1);
+  });
+});
