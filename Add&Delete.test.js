a706eb02e38f1dd69ba91d26d6cf17b828b6b572
Hectorjte <hectorjte@gmail.com>
2022-02-24 08:50:35 -0600

    Add babel

--- Add&Delete.test.js
+++ Add&Delete.test.js
@@ -1,15 +1,20 @@
-import List from './ToDoList.js';
-import Container from './Container.js';
+/**
+ * @jest-environment jsdom
+ */
+import List from './src/ToDoList.js';
+import Container from './src/Container.js';
 
 describe('ToDoList localstorage', () => {
   test('Delete', () => {
+    window.localStorage.clear();
     const ToDoList = new List();
     ToDoList.AddToDO('New Task 1');
     ToDoList.AddToDO('New Task 2');
-    ToDoList.deleteTask(1);
+    ToDoList.RemoveToDo(1);
     expect(ToDoList.ToDo.length).toBe(1);
   });
   test('Add', () => {
+    window.localStorage.clear();
     const ToDoList = new List();
     ToDoList.AddToDO('New event');
     ToDoList.AddToDO('New event 2');
@@ -18,7 +23,8 @@ describe('ToDoList localstorage', () => {
 });
 
 describe('DOM manipulation', () => {
-  test('Add item', () => {
+  test('Add T\to-do', () => {
+    window.localStorage.clear();
     document.body.innerHTML = `
     <template id='ToDo' >
       <div class='row toDoR' draggable='true'>
@@ -33,36 +39,37 @@ describe('DOM manipulation', () => {
     <div class='container'>
     </div>
     `;
-    const List = new Container();
-    List.ToDoList.AddToDO('event');
-    List.ToDoList.AddToDO('event 1');
-    List.ToDoList.AddToDO('event 2');
-    List.ShowList();
+    const cont = new Container();
+    cont.List.AddToDO('event');
+    cont.List.AddToDO('event 1');
+    cont.List.AddToDO('event 2');
+    cont.ShowList();
     const tasks = document.body.querySelectorAll('.toDoR');
     expect(tasks.length).toBe(3);
   });
-  test('Delete item', () => {
+  test('Delete to-do', () => {
+    window.localStorage.clear();
     document.body.innerHTML = `
     <template id='ToDo' >
-      <div class='row toDoR' draggable='true'>
-        <div class='task'>
-          <input type='checkbox' class='checkbox'>
-          <input class='toDoName' type='text'>
-        </div>
-        <button class='btnMove' type='button'>&#8942;</button>
-        <button class='btnRemove' type='button'>&#128465;</button>
-      </div>
+    <div class='row toDoR' draggable='true'>
+    <div class='task'>
+    <input type='checkbox' class='checkbox'>
+    <input class='toDoName' type='text'>
+    </div>
+    <button class='btnMove' type='button'>&#8942;</button>
+    <button class='btnRemove' type='button'>&#128465;</button>
+    </div>
     </template>
     <div class='container'>
     </div>
     `;
-    window.localStorage.clear();
-    const List = new Container();
-    List.ToDoList.AddToDO('event');
-    List.ToDoList.AddToDO('event 1');
-    List.ToDoList.RemoveToDo(0);
-    List.ShowList();
+    const cont = new Container();
+    cont.List.AddToDO('event');
+    cont.List.AddToDO('event 1');
+    cont.List.AddToDO('event 2');
+    cont.List.RemoveToDo(1);
+    cont.ShowList();
     const tasks = document.body.querySelectorAll('.toDoR');
-    expect(tasks.length).toBe(1);
+    expect(tasks.length).toBe(2);
   });
 });
